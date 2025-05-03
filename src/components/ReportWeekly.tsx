98cc0d359a9bcdc9db07d4e18f1b5bf4a0586843
FE_juncci <81576126+Debuging-JunSeoPark@users.noreply.github.com>
2025-05-03 16:42:27 +0900

    feat: 위클리/먼슬리 탭바 컴포넌트 구현 (#6)
    * 레포트 페이지에 주간/월간 보기 전환 기능 구현
    
    * style: 텍스트 색상 통일

---
+++ src/components/ReportWeekly.tsx
@@ -0,0 +1,10 @@
+function ReportWeekly() {
+    return (
+      <div className="text-center text-sm text-gray-600">
+        Weekly report content goes here
+      </div>
+    );
+  }
+  
+  export default ReportWeekly;
+  
