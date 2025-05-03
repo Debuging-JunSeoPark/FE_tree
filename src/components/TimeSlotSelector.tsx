c2144197779474ef7871f0322d61d2cf5c828cd9
sungwonnoh <jessica20730@naver.com>
2025-05-03 17:59:14 +0900

    feat: 시간대 선택 ui

---
+++ src/components/TimeSlotSelector.tsx
@@ -0,0 +1,40 @@
+import treeMorning from "../assets/images/treeMorning.png";
+import treeLunch from "../assets/images/treeLunch.png";
+import treeEvening from "../assets/images/treeEvening.png";
+type Props = {
+  selected: string | null;
+  setSelected: (label: string) => void;
+};
+
+function TimeSlotSelector({ selected, setSelected }: Props) {
+  const timeSlots = [
+    { id: 1, label: "Morning", count: "1/3", src: treeMorning },
+    { id: 2, label: "Lunch", count: "2/3", src: treeLunch },
+    { id: 3, label: "Evening", count: "3/3", src: treeEvening },
+  ];
+  return (
+    <div className="flex flex-row items-center justify-around gap-2 m-2">
+      {timeSlots.map((slot) => {
+        const isSelected = selected == slot.label;
+        return (
+          <div
+            key={slot.id}
+            onClick={() => setSelected(slot.label)}
+            className={`flex flex-col items-center justify-center p-1 w-29 h-22  rounded-[6px] border-2 ${
+              isSelected ? "bg-second border-main" : "bg-white border-gray-100"
+            } font-PRegular text-[10px]`}
+          >
+            <img
+              src={slot.src}
+              alt={slot.label}
+              className="rounded-full w-12 h-12"
+            />
+            <div>{slot.label}</div>
+            <div>{slot.count}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+export default TimeSlotSelector;
--- src/pages/Record.tsx
+++ src/pages/Record.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+import TimeSlotSelector from "../components/TimeSlotSelector";
 import Weekly from "../components/Weekly";
 
 function Record() {
+  const [selected, setSelected] = useState<string | null>("Morning");
   return (
     <div>
       <Weekly />
+      <TimeSlotSelector selected={selected} setSelected={setSelected} />
+      <div className="flex flex-col">
+        <div className="font-PSemiBold text-lg text-Title">
+          {selected} Reflection
+        </div>
+        <div className="font-PMedium text-xs text-sub">
+          Wrap up your day by sharing your thoughts
+        </div>
+      </div>
     </div>
   );
 }
