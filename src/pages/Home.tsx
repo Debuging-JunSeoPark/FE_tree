32c77ec399788f27b11f4a1467f7b3c0d73a5de1
sungwonnoh <jessica20730@naver.com>
2025-05-04 17:45:45 +0900

    feat: TreeTrunk 컴포넌트 파일 분리

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,8 @@
 import icon from "../assets/images/Icon.png";
+import TreeTrunk from "../components/TreeTrunk";
 function Home() {
   return (
-    <div className="flex flex-col justify-around items-center h-full gap-4">
+    <div className="flex flex-col justify-center items-center h-full gap-4">
       <div className="flex flex-row items-center p-3 gap-2 w-84 h-14.5 rounded-xl border-2 border-homeBorder">
         <img
           src={icon}
@@ -11,21 +12,13 @@ function Home() {
         <div className="font-PBold text-base text-black">Your Nickname</div>
       </div>
       <div>
-        <div className="text-base text-black font-PMedium">
+        <div className="text-base text-black font-PMedium pl-2 mb-1">
           Questions Challenge
         </div>
-        <div className="flex flex-col items-center gap-2 w-84 h-101 rounded-xl border-2 border-homeBorder shadow-lg">
-          <div className="w-full h-84 rounded-t-xl bg-sub">tree trunk</div>
-          <span className="flex flex-col items-end justify-end w-full mr-4 ">
-            <div className="font-PLight text-xs">Daily Progress Visual</div>
-            <div className="font-PExtraBold text-main font-base">
-              1/21 answered
-            </div>
-          </span>
-        </div>
+        <TreeTrunk />
       </div>
       <div>
-        <div className="text-base text-black font-PMedium">
+        <div className="text-base text-black font-PMedium pl-2 mb-1">
           Quote of the Day
         </div>
         <div className="flex flex-row items-center p-3 gap-2 w-84 h-32 rounded-xl border-2 border-homeBorder shadow-lg"></div>
