72e577a65d9346dfec0fa9c7721ae4fdf94f8f32
sungwonnoh <jessica20730@naver.com>
2025-05-11 00:35:42 +0900

    feat:사용자 정보 불러오기 api

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,46 @@
 import { useEffect, useState } from "react";
-import icon from "../assets/images/Icon.png";
 import TreeTrunk from "../components/TreeTrunk";
-import Cookies from "js-cookie";
 import QuoteCard from "../components/QuoteCard";
+import greenIcon from "../assets/images/treeIcon.svg";
+import yellowIcon from "../assets/images/forsythiaIcon.svg";
+import pinkIcon from "../assets/images/cherryBlossomIcon.svg";
+import { getUserProfile } from "../apis/user";
 
 function Home() {
   const [nickname, setNickname] = useState<string>("");
-
-  useEffect(() => {
-    const userNickname = Cookies.get("nickname");
-    if (userNickname) {
-      setNickname(userNickname);
+  const [avatar, setAvatar] = useState<string>("");
+  const getAvatarImage = (avatar: string | null): string => {
+    switch (avatar) {
+      case "GREEN":
+        return greenIcon;
+      case "YELLOW":
+        return yellowIcon;
+      case "PINK":
+        return pinkIcon;
+      default:
+        return greenIcon;
     }
+  };
+  useEffect(() => {
+    const userInfo = async () => {
+      try {
+        const user = await getUserProfile();
+        setNickname(user.nickname);
+        setAvatar(user.avatar);
+      } catch (error) {
+        console.error("유저 정보 불러오기 실패");
+        throw error;
+      }
+    };
+    userInfo();
   }, []);
+
   const answeredCount = 90;
   return (
     <div className="flex flex-col justify-center items-center h-full gap-4">
       <div className="flex flex-row items-center p-3 gap-2 w-84 h-14.5 rounded-xl border-2 border-homeBorder">
         <img
-          src={icon}
+          src={getAvatarImage(avatar)}
           alt="아이콘"
           className="w-10 h-10 rounded-full border-1 border-[#80A94D]"
         />
@@ -37,8 +59,8 @@ function Home() {
           Quote of the Day
         </div>
         <div className="flex flex-col justify-center p-3 gap-2 w-84 h-32 rounded-xl border-2 border-homeBorder shadow-lg bg-white">
-  <QuoteCard />
-</div>
+          <QuoteCard />
+        </div>
       </div>
     </div>
   );
