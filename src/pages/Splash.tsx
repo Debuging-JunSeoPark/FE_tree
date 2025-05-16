0e9c8b11f9b8784c112876b67890bff5ff37b651
sungwonnoh <jessica20730@naver.com>
2025-05-16 15:58:26 +0900

    modify: 스플래시뷰 화면 크기 조정

--- src/pages/Splash.tsx
+++ src/pages/Splash.tsx
@@ -14,32 +14,36 @@ function Splash() {
   }, []);
 
   return (
-    <div className="w-full h-screen flex flex-col items-center justify-center bg-gradient-to-b from-main to-[#964B00]">
-      {/* 로고: 위치 고정 */}
-      <img
-        src={splashlogo}
-        alt="logo"
-        className="w-32 h-auto mb-4"
-        style={{
-          transition: "none",
-          transform: "none",
-        }}
-      />
+    <div>
+      <div className="w-full max-w-md mx-auto h-screen flex flex-col items-center justify-center bg-gradient-to-b from-main to-[#964B00]">
+        {/* 로고: 위치 고정 */}
+        <img
+          src={splashlogo}
+          alt="logo"
+          className="w-32 h-auto mb-4"
+          style={{
+            transition: "none",
+            transform: "none",
+          }}
+        />
 
-      {/* 텍스트: 등장 전에도 자리 확보 */}
-      <div className="flex space-x-1 text-white text-lg font-PMedium min-h-[1.5rem]">
-        {showText &&
-          text.split("").map((char, idx) => (
-            <span
-              key={idx}
-              style={{
-                opacity: 0,
-                animation: `fadeInLetter 0.1s ease-out ${idx * 0.03}s forwards`,
-              }}
-            >
-              {char === " " ? "\u00A0" : char}
-            </span>
-          ))}
+        {/* 텍스트: 등장 전에도 자리 확보 */}
+        <div className="flex space-x-1 text-white text-lg font-PMedium min-h-[1.5rem]">
+          {showText &&
+            text.split("").map((char, idx) => (
+              <span
+                key={idx}
+                style={{
+                  opacity: 0,
+                  animation: `fadeInLetter 0.1s ease-out ${
+                    idx * 0.03
+                  }s forwards`,
+                }}
+              >
+                {char === " " ? "\u00A0" : char}
+              </span>
+            ))}
+        </div>
       </div>
     </div>
   );
